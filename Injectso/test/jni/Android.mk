dec2b22a1e079f9c4ac32959f462987d0d554657
XK <changkk@coralsec.com>
2016-06-10 18:25:50 +0800

    *工程文件整理

--- Injectso/test/jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)  
-  
-include $(CLEAR_VARS)  
-  
-LOCAL_LDLIBS += -L$(SYSROOT)/usr/lib -llog   
-#LOCAL_ARM_MODE := arm  
-LOCAL_MODULE    := hello  
-LOCAL_SRC_FILES := hello.c  
-include $(BUILD_SHARED_LIBRARY) 
--- got-hook/hook-fopen/jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)  
-  
-include $(CLEAR_VARS)  
-  
-LOCAL_LDLIBS += -L$(SYSROOT)/usr/lib -llog   
-#LOCAL_ARM_MODE := arm  
-LOCAL_MODULE    := libhello
-LOCAL_SRC_FILES := HookModule.c
-include $(BUILD_SHARED_LIBRARY) 
